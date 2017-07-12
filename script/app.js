79d13ff8880273148c2c2f2311484735c94e9092
Yusaku Hieda <y.hieda@pocket-soft.co.jp>
2017-07-13 00:28:35 +0900

    フォーマット変更

--- script/app.js
+++ script/app.js
@@ -10,9 +10,9 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -26,5 +26,5 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 
 bot.dialog('/', function (session) {
-   session.send('Hello World'); 
+    session.send('Hello World');
 });
